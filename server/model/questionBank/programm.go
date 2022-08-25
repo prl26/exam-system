714fe68d46194b6ffd90470dbd53d7f2cbe04e69
prl26 <1344211589@qq.com>
2022-08-25 16:11:22 +0800

    删除题目类型

--- server/model/questionBank/programm.go
+++ server/model/questionBank/programm.go
@@ -10,7 +10,6 @@ type Programm struct {
 	global.GVA_MODEL
 	Describe     string `json:"describe" form:"describe" gorm:"column:describe;comment:;"`
 	Title        string `json:"title" form:"title" gorm:"column:title;comment:;"`
-	ProblemType  *int   `json:"problemType" form:"problemType" gorm:"column:problem_type;comment:难度;size:8;"`
 	QuestionType *int   `json:"questionType" form:"paperId" gorm:"column:question_type;comment:题目类型;size:8;"`
 }
 
