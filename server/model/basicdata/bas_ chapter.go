ee3993d8c81476ce44541dc1fa368ccf1589859b
AloneAtWar <2318583515@qq.com>
2023-02-01 22:37:38 +0800

    修复课程与阶段无法创建的bug

--- server/model/basicdata/bas_ chapter.go
+++ server/model/basicdata/bas_ chapter.go
@@ -12,8 +12,8 @@ type Chapter struct {
 	Name          string                  `json:"name" form:"name" gorm:"column:name;comment:章节名称;size:32;"`
 	LessonId      *int                    `json:"lessonId" form:"lessonId" gorm:"column:lesson_id;comment:所属课程id;size:32;"`
 	Knowledges    []*lessondata.Knowledge `json:"knowledges" gorm:"foreignKey:ChapterId"`
-	QuestionCount *int64                  `json:"questionCount,omitempty"`
-	DoneCount     *int64                  `json:"doneCount,omitempty"`
+	QuestionCount *int64                  `json:"questionCount,omitempty"   gorm:"-"`
+	DoneCount     *int64                  `json:"doneCount,omitempty" gorm:"-"`
 }
 
 // TableName Chapter 表名
